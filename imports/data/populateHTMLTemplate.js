654aa0872fdfb3d2b87655fb841d9db7b04eebfb
Radovan Šurlák <me@radovansurlak.com>
2020-07-07 14:16:39 +0200

    fixed up certifications, fit and cut and model height and size

--- imports/data/populateHTMLTemplate.js
+++ imports/data/populateHTMLTemplate.js
@@ -24,19 +24,19 @@ const defaults = {
   },
 };
 
-function createCertificationsLine(product) {
-  const certificationKeys = [
-    'productionCertifications',
-    'materialCertifications',
-    'brandCertifications',
-    'productCertifications',
-  ];
-  const certificationStrings = [];
-  certificationKeys.forEach(
-    key => product[key] && certificationStrings.push(product[key]),
-  );
-  return `<li>Certifications: ${certificationStrings.join(', ')}</li>`;
-}
+// function createCertificationsLine(product) {
+//   const certificationKeys = [
+//     'productionCertifications',
+//     'materialCertifications',
+//     'brandCertifications',
+//     'productCertifications',
+//   ];
+//   const certificationStrings = [];
+//   certificationKeys.forEach(
+//     (key) => product[key] && certificationStrings.push(product[key]),
+//   );
+//   return `<li>Certifications: ${certificationStrings.join(', ')}</li>`;
+// }
 
 function populateHTMLTemplate(
   product = defaults.product,
@@ -45,32 +45,16 @@ function populateHTMLTemplate(
 ) {
   const lines = ['<ul>'];
 
-  if (product.modelName && product.modelSizeWorn) {
-    lines.push(
-      `<li>Le modèle ${product.modelName} porte une taille ${product.modelSizeWorn}.</li>`,
-    );
-  } else if (product.modelSizeWorn) {
-    lines.push(
-      `<li>Le modèle porte une taille ${product.modelSizeWorn}.</li>`,
-    );
-  }
-  if (product.cut) {
-    lines.push(`<li>Coupe: ${product.cut}</li>`);
-  }
-  if (product.fit) {
-    lines.push(`<li>Fit: ${product.fit}</li>`);
-  }
-  if (product.color) {
-    lines.push(`<li>Couleur: ${product.color}</li>`);
-  }
-  if (product.material) {
-    lines.push(`<li>Matières: ${product.material}</li>`);
-  }
-  if (product.madeIn) {
-    lines.push(`<li>Made in: ${product.madeIn}</li>`);
-  }
+  if (product.modelSizeWorn) lines.push(`<li>Le modèle ${product.modelName} porte une taille ${product.modelSizeWorn}.</li>`);
+  if (product.modelHeight) lines.push(`<li>Le modèle mesure ${product.modelHeight}.</li>`);
+  if (product.details) lines.push(`<li>Détails: ${product.details}</li>`);
+  if (product.fitAndCut) lines.push(`<li>Fit et coupe: ${product.fitAndCut}</li>`);
+  if (product.color) lines.push(`<li>Couleur: ${product.color}</li>`);
+  if (product.material) lines.push(`<li>Matières: ${product.material}</li>`);
+  if (product.madeIn) lines.push(`<li>Made in: ${product.madeIn}</li>`);
+  if (product.certifications) lines.push(`<li>Certifications: ${product.certifications}</li>`);
+
   lines.push("<li>Entretien: Éviter de laver trop souvent, préférer l'aération et le pressing. Sinon lavage à froid, 20° ou 30° pour économiser un maximum l'énergie. </li>");
-  lines.push(createCertificationsLine(product));
 
   lines.push('</ul>');
 
